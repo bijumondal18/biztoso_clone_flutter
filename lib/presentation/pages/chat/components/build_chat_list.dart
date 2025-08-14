28fb6dc5bab9a49354b90f07a2c89edf02b38190
Biju Mondal <biju@biztoso.com>
2025-08-14 11:22:12 +0530

    fixed chat list UI

--- lib/presentation/pages/chat/components/build_chat_list.dart
+++ lib/presentation/pages/chat/components/build_chat_list.dart
@@ -17,84 +17,93 @@ class _BuildChatListState extends State<BuildChatList> {
   Widget build(BuildContext context) {
     return ListView.separated(
       itemCount: 10,
-      padding: EdgeInsets.all(AppSizes.kDefaultPadding),
+      padding: EdgeInsets.symmetric(vertical: AppSizes.kDefaultPadding),
       itemBuilder: (context, index) {
-        return Row(
-          spacing: AppSizes.kDefaultPadding,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            ProfileAvatar(
-              imageUrl:
-                  'https://static-cse.canva.com/blob/2121234/1600w-B-cRyoh7b98.jpg',
-              radius: 44,
-              onPressed: () {},
+        return InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppSizes.kDefaultPadding,
+              vertical: 4.0,
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 2.0,
-                children: [
-                  Text(
-                    'Kushal Kumar',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    'Hi, This is the last message sent by this user.',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(
-                        context,
-                      ).colorScheme.surfaceContainer.withAlpha(180),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
+            child: Row(
+              spacing: AppSizes.kDefaultPadding,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Text(
-                  'Aug 14',
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(
-                      context,
-                    ).colorScheme.surfaceContainer.withAlpha(180),
+                ProfileAvatar(
+                  imageUrl:
+                      'https://static-cse.canva.com/blob/2121234/1600w-B-cRyoh7b98.jpg',
+                  radius: 44,
+                  onPressed: () {},
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 2.0,
+                    children: [
+                      Text(
+                        'Kushal Kumar',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Hi, This is the last message sent by this user.',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(
+                            context,
+                          ).colorScheme.surfaceContainer.withAlpha(180),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(AppSizes.kDefaultPadding / 3),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  child: Text(
-                    '3',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                      color: AppColors.white,
-                      fontWeight: FontWeight.w600,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Aug 14',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        fontWeight: FontWeight.w600,
+                        color: Theme.of(
+                          context,
+                        ).colorScheme.surfaceContainer.withAlpha(180),
+                      ),
                     ),
-                  ),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(AppSizes.kDefaultPadding / 3),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      child: Text(
+                        '3',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                          color: AppColors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         );
       },
       separatorBuilder: (BuildContext context, int index) {
         return Padding(
           padding: const EdgeInsets.only(left: 62),
-          child: HorizontalDivider(height: 10),
+          child: HorizontalDivider(),
         );
       },
     );
