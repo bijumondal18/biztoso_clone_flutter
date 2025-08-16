94da0e93a8af3d48d2ce6ce81888566aa97bce6f
Biju Mondal <biju@biztoso.com>
2025-08-16 22:35:32 +0530

    fixed

--- lib/presentation/pages/home/widgets/post_card.dart
+++ lib/presentation/pages/home/widgets/post_card.dart
@@ -1,4 +1,8 @@
 import 'package:biztoso/core/themes/app_sizes.dart';
+import 'package:biztoso/presentation/widgets/profile_avatar.dart';
+import 'package:biztoso/utils/app_utils.dart';
+import 'package:biztoso/utils/date_time_utils.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../data/models/post_model.dart';
@@ -10,6 +14,8 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    final height = MediaQuery.sizeOf(context).height;
     final isReshare = post.originalPost != null;
 
     return Container(
@@ -21,51 +27,87 @@ class PostCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: Theme.of(context).cardColor,
         borderRadius: BorderRadius.circular(AppSizes.cardCornerRadius),
-        border: Border.all(width: 1.0, color: Theme.of(context).dividerColor),
+        border: Border.all(
+          width: 1.0,
+          color: Theme.of(context).dividerColor.withAlpha(100),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).shadowColor.withAlpha(200),
+            blurRadius: AppSizes.blurRadius,
+          ),
+        ],
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // header
           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: AppSizes.kDefaultPadding / 2,
             children: [
-              CircleAvatar(
-                radius: 20,
-                backgroundImage:
-                    post.profilePic != null && post.profilePic!.isNotEmpty
-                    ? NetworkImage(post.profilePic ?? '')
-                    : null,
-                child: (post.profilePic == null || post.profilePic!.isEmpty)
-                    ? const Icon(Icons.person_outline)
-                    : null,
+              ProfileAvatar(
+                imageUrl: '${post.originalPost?.ownerDetails?.ownerProfile}',
+                onPressed: () {},
               ),
-              const SizedBox(width: 10),
               Expanded(
-                child: Text(
-                  post.userName ?? 'Unknown',
-                  style: Theme.of(context).textTheme.titleMedium,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppUtils.firstNonEmptyTitle([
+                        post.originalPost?.ownerDetails?.ownerName,
+                      ]),
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      DateTimeUtils.formatChatTimestamp(post.createdAt ?? ''),
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                        color: Theme.of(context).hintColor,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              // Text(
-              //   _compactTime(post.createdAt),
-              //   style: Theme
-              //       .of(context)
-              //       .textTheme
-              //       .bodySmall,
-              // ),
+              PopupMenuButton(
+                icon: Icon(
+                  Icons.more_vert_rounded,
+                  color: Theme.of(context).colorScheme.surfaceContainer,
+                ),
+                itemBuilder: (context) {
+                  return [PopupMenuItem(child: Text('Save'))];
+                },
+              ),
             ],
           ),
-          const SizedBox(height: 8),
 
           // description
-          if ((post.description ?? '').isNotEmpty) Text(post.description ?? ''),
+          Visibility(
+            visible: post.description != null && post.description!.isNotEmpty,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: AppSizes.kDefaultPadding / 2,
+              ),
+              child: Text(
+                post.description ?? '',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ),
 
           // images (first only for demo)
           if (post.images.isNotEmpty) ...[
             const SizedBox(height: 8),
             ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(post.images.first, fit: BoxFit.cover),
+              borderRadius: BorderRadius.circular(AppSizes.cardCornerRadius),
+              child: CachedNetworkImage(
+                imageUrl: post.images.first,
+                fit: BoxFit.fitWidth,
+                height: height * 0.4,
+                width: width,
+              ),
             ),
           ],
 
